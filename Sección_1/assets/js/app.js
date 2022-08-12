5a6e7aaa50b674dbb621bd98956c1ad045669c90
DianaB177 <anaidb77@gmail.com>
2022-08-12 23:40:34 +0200

    Practicas basicas

---
+++ Sección_1/assets/js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//alert('Hola mundo');
+//let respuesta = prompt('¿Cual es tu nombre?');
+//console.log('respuesta');
+//console.log('****' + respuesta + '****');
+
+//const seleccion = confirm('¿Está seguro?');
+//console.log(seleccion);
+
+console.log (global);
