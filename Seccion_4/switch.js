09e9ad6fa27b1b6ac233e9ab19ca4d028d2414cc
DianaB177 <anaidb77@gmail.com>
2022-08-21 15:02:13 +0200

    while

---
+++ Seccion_4/switch.js
@@ -0,0 +1,27 @@
+
+const hoy = new Date();
+let dia = hoy.getDay();
+
+switch (dia) {
+    case 0:
+        console.log('Domingo');
+        break;
+    case 1:
+        console.log('Lunes');
+        break;
+    case 2:
+        console.log('Martes');
+        break;    
+    case 3:
+        console.log('Miercoles');
+        break;
+    case 4:
+        console.log('Jueves');
+        break;
+    case 5:
+        console.log('Viernes');
+        break;
+    default:
+        console.log('Sábado');
+        break;
+}
