a60b5c92d6f51a0fa2f5d88bc1a5677e3303a8ac
DianaB177 <anaidb77@gmail.com>
2022-08-21 16:59:23 +0200

    terminada la base

---
+++ Seccion_4/ciclos/while-do-while.js
@@ -0,0 +1,19 @@
+const carros = ['Porche', 'Toyota','Nissan', 'Macallan'];
+let i = 0;
+
+//while (i < carros.length) {
+//   console.log(carros[i]);
+//    i ++;
+//}
+
+while (carros[i]) {
+    console.log(carros[i]);
+    i ++; 
+}
+console.warn('Do While');
+
+let j = 0;
+do {
+    console.log(carros[j]);
+   j++; 
+} while (carros[j]);
--- ciclos/while-do-while.js
+++
@@ -1,7 +0,0 @@
-const carros = ['Porche', 'Toyota','Nissan', 'Macallan'];
-let i = 0;
-
-while (i < carros.length) {
-    console.log(carros[i]);
-    i ++;
-}
